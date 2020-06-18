07e4300bfb7cf028aa2fa69618865eb7f11491f3
Yair Carreno <yaircarreno@gmail.com>
2020-06-18 07:32:50 -0500

    Initial Commit

---
+++ CleanWayRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanWayRx
+//
+//  Created by Yair Carreno on 18/06/20.
+//  Copyright © 2020 Yair Carreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
